ed0385156e3c603b4f6b70e8475ff4197cc4cbb1
davidboers <kwmmineccraft@gmail.com>
2025-08-05 14:51:42 -0400

    Explanation

--- random-password.c
+++ random-password.c
@@ -2,6 +2,10 @@
 #include <stdlib.h>
 #include <time.h>
 
+/* Generates a random password, composed of between 8 and 26 characters, numbers and letters (uppercase and lowercase).
+ * Even one of the shorter passwords will take years to guess.
+ */
+
 int main(void)
 {
     srand(time(NULL));
@@ -9,7 +13,7 @@ int main(void)
     const int max_len = 26;
     const int min_len = 8;
     const int length = rand() % (max_len - min_len + 1) + min_len;
-    
+
     const int no_chars = 26 + 26 + 10;
     char bank[no_chars];
     int n_char = 0;
